3ec72e1c465f304a2176205a44de28e896642b0a
pravinau26 <pravinau26@gmail.com>
2026-08-05 06:46:41 +0000

    Update Terraform EC2 instance_type to t3.micro for us-east-1 Free Tier policy compliance

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,7 +18,7 @@ variable "public_subnet_cidr" {
 
 variable "instance_type" {
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
   description = "EC2 instance size for Kubernetes/Helm deployment (Free Tier Eligible)"
 }
 
