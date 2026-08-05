1fc8d76b3fecfae1a32d418936c345f9585fa06b
pravinau26 <pravinau26@gmail.com>
2026-08-05 06:14:44 +0000

    Update Terraform with Elastic IP (aws_eip), Helm specs, and GitHub Actions CI/CD

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,8 +14,8 @@ output "ec2_instance_id" {
 }
 
 output "ec2_public_ip" {
-  value       = aws_instance.app_server.public_ip
-  description = "The public IP address of the EC2 instance"
+  value       = aws_eip.app_server_eip.public_ip
+  description = "The static Elastic IP address of the EC2 instance"
 }
 
 output "s3_bucket_website_endpoint" {
