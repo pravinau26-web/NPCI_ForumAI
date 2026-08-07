dcf758b2dbae56bcd2fff3ec0e60fc582cf46239
Pravin <pravinau26@gmail.com>
2026-08-07 05:30:03 +0000

    fix: allocate static Elastic IP (EIP) for EC2 and generate workflow deployment links summary

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,8 +14,8 @@ output "ec2_instance_id" {
 }
 
 output "ec2_public_ip" {
-  value       = aws_instance.app_server.public_ip
-  description = "The public IP address of the primary EC2 instance"
+  value       = aws_eip.app_eip.public_ip
+  description = "The static Elastic IP address of the primary EC2 instance"
 }
 
 output "ec2_ssh_private_key" {
